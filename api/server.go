3f17db557946c4892c66d41ec4d8e87c6d2697de
Flávio Ferreira <business.flavioferreira@gmail.com>
2022-04-26 07:40:42 -0300

    transfer handler

--- api/server.go
+++ api/server.go
@@ -14,9 +14,11 @@ func NewServer(store db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
-	router.POST("accounts", server.createAccount)
-	router.GET("accounts/:id", server.getAccount)
-	router.GET("accounts", server.listAccounts)
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts", server.listAccounts)
+
+	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
 	return server
